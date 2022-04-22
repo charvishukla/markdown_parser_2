8a9915a3bdcd4cb61c527898626f9bfa6b310d3b
Georgexli3 <103277409+Georgexli3@users.noreply.github.com>
2022-04-22 11:20:25 -0700

    tester file works

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class MarkdownParse {
 
-    public static ArrayList<String> getLinks(String markdown) {
+    public static ArrayList<String> getLinksNew(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
@@ -30,11 +30,31 @@ public class MarkdownParse {
         return toReturn;
     }
 
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
+            int openBracket = markdown.indexOf("[", currentIndex);
+            System.out.println(openBracket);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            System.out.println(closeBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            System.out.println(openParen);
+            int closeParen = markdown.indexOf(")", openParen);
+            System.out.println(closeParen);
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
+        }
+
+        return toReturn;
+    }
+
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
+        ArrayList<String> links = getLinksNew(content);
 	    System.out.println(links);
     }
 }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,23 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void testGetLinks() throws IOException{
+        //ArrayList<String> res = MarkdownParse.getLinks("test-file.md");
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expectedList = new ArrayList<String>();
+        expectedList.add("https://something.com");
+        expectedList.add("some-thing.html");
+        assertEquals(expectedList,MarkdownParse.getLinks(content));
     }
 }
