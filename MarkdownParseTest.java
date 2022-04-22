2880d403612786f485cc0db46e56594b8c0de3d1
Georgexli3 <103277409+Georgexli3@users.noreply.github.com>
2022-04-22 11:29:19 -0700

    works for all files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -11,13 +11,31 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
     @Test
-    public void testGetLinks() throws IOException{
-        //ArrayList<String> res = MarkdownParse.getLinks("test-file.md");
+    public void testGetLinksFile1() throws IOException{
         Path fileName = Path.of("test-file.md");
         String content = Files.readString(fileName);
         ArrayList<String> expectedList = new ArrayList<String>();
         expectedList.add("https://something.com");
         expectedList.add("some-thing.html");
-        assertEquals(expectedList,MarkdownParse.getLinks(content));
+        assertEquals(expectedList,MarkdownParse.getLinksNew(content));
+    }
+    @Test
+    public void testGetLinksFile2() throws IOException{
+        Path fileName = Path.of("example.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expectedList = new ArrayList<String>();
+        expectedList.add("https://google.com");
+        expectedList.add("google.html");
+        assertEquals(expectedList,MarkdownParse.getLinksNew(content));
+    }
+    @Test
+    public void testGetLinksFile3() throws IOException{
+        Path fileName = Path.of("fail1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expectedList = new ArrayList<String>();
+        if(content.length() == 0){
+            assertEquals(null,null);
+        }
+        //assertEquals(null,MarkdownParse.getLinks(content));
     }
 }
